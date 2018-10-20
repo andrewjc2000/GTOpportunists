02338a3de71ceae75fd4503676688ea37d0f7d10
Andrew Chafos <andrewjchafos@gmail.com>
2018-10-20 18:14:30 -0400

    Fixed collisions and other things

--- Platformer/mechanics.js
+++ Platformer/mechanics.js
@@ -2,6 +2,9 @@
 var Game = {
 	running: true,
 	keys: ["w", "a", "d"],
+	init: function(){
+
+	},
 	handleKeyPress: function(keyEvent){
 		if(Game.keys.indexOf(keyEvent.key) !== -1){
 			var dir = Game.keys.indexOf(keyEvent.key);
@@ -49,129 +52,124 @@ var Game = {
 };
 
 var Player = {
-	playerWidth: 5,
-	playerHeight: 10,
+	playerWidth: 3,
+	playerHeight: 15,
 	sidewaysAmount: .3,
 	upwardsAmount: 30,
 	movingLeft: false,
 	movingRight: false,
 	jumping: false,
+	falling: false,
+	fallProgress: 0,
 	jumpHeight: 20,
 	jumpProgress: 0,
 	onBarrier: false,
-	baseY: 80,
+	groundY: 75,
+	baseY: 75,
 	x: 0,
-	y: 80,
+	y: 75,
 	updatePosition: function () {
 
-		if(Player.onBarrier){
-			var testX = Player.x + Player.playerWidth / 2;
-			var testY = Player.y + Player.playerHeight;
-			var platform = Barriers.insideBarrier(testX, testY);
-			if(!platform) {
-				if(!Player.jumping){
-					Player.jumping = true;
-					Player.jumpProgress = 50;
-					Player.baseY = 80;
-					Player.onBarrier = false;
-					//alert('falling');
-				}
+		if(Player.movingLeft) {
+			if(Player.x > 0) {
+				Player.x -= Player.sidewaysAmount;
 			} else {
-				if(Player.jumping) {
-					if(Player.jumpProgress < 100){
-						Player.jumpProgress++;
-						var h = Player.upwardsAmount;
-						var p = Player.jumpProgress;
-						Player.y = Player.baseY - ((-h/2500) * (p * p) + (h/25) * p);
-					} 
-					else {
-						Player.jumping = false;
-						Player.jumpProgress = 0;
-						Player.y = Player.baseY;
-					}
-				}
-				if(Player.movingLeft) {
-					if(Player.x > 0) {
-						Player.x -= Player.sidewaysAmount;
-					} else {
-						Player.x = 0;
-						Player.movingLeft = false;
-					}
-				}
-				if(Player.movingRight) {
-					if(Player.x < 100 - Player.playerWidth) {
-						Player.x += Player.sidewaysAmount;
-					} else {
-						Player.x = 100 - Player.playerWidth;
-						Player.movingRight = false;
-					}
-				}
+				Player.x = 0;
+				Player.movingLeft = false;
 			}
 		}
-		else {
-			if(Player.jumping) {
-				var testX = Player.x + Player.playerWidth;
-				var testY = Player.y + Player.playerHeight;
-				var platform = Barriers.insideBarrier(testX, testY);
-				if (!platform) {
-					if(Player.jumpProgress < 100){
-						Player.jumpProgress++;
-						var h = Player.upwardsAmount;
-						var p = Player.jumpProgress;
-						Player.y = Player.baseY - ((-h/2500) * (p * p) + (h/25) * p);
-					}
-					else {
-						Player.jumping = false;
-						Player.jumpProgress = 0;
-					}
-				}
-				else {
-					Player.jumping = false;
-					Player.jumpProgress = 0;
-					Player.y = platform[1] - Player.playerHeight;
-					Player.onBarrier = true;
-					Player.baseY = Player.y;
+		if(Player.movingRight) {
+			if(Player.x < 100 - Player.playerWidth) {
+				Player.x += Player.sidewaysAmount;
+			} else {
+				Player.x = 100 - Player.playerWidth;
+				Player.movingRight = false;
+			}
+		}
+
+		var testX = Player.x + Player.playerWidth / 2;
+		var testY = Player.y + Player.playerHeight;
+		$("#box").css("left", testX + "%");
+		$("#box").css("top", testY + "%");
+		var platform = Barriers.insideBarrier(testX, testY);
+
+		if (!platform && Player.onBarrier && !Player.falling && !Player.jumping) {
+			Player.onBarrier = false;
+			Player.falling = true;
+			Player.fallProgress = 100;
+			Player.y = Player.baseY - ((-h/2500) * (p * p) + (h/25) * p);
+		}
+
+		if (Player.falling) {
+			if (platform) {
+				Player.falling = false;
+				Player.fallProgress = 0;
+				Player.onBarrier = true;
+				Player.y = platform[2] - Player.playerHeight;
+				Player.baseY = Player.y;
+			} else if (Player.y + Player.playerHeight >= Player.groundY) {
+				Player.placeOnGround();
+			} else {
+				Player.fallProgress++;
+				var h = Player.upwardsAmount;
+				var p = Player.fallProgress;
+				var y = Player.baseY - ((-h/2500) * (p * p) + (h/25) * p);
+				if (y + Player.playerHeight >= Player.groundY) {
+					Player.placeOnGround();
+				} else {
+					Player.y = y;
 				}
 			}
-			if(Player.movingLeft) {
-				if(Player.x > 0) {
-					var testX = Player.x;
-					var testY = Player.y + Player.playerHeight;
-					if (!Barriers.insideBarrier(testX, testY)) {
-						Player.x -= Player.sidewaysAmount;
-					} 
-				} else {
-					Player.x = 0;
-					Player.movingLeft = false;
-				}
+
+		} 
+		if (Player.jumping) {
+			if (platform && Player.jumpProgress >= 50) {
+				Player.jumping = false;
+				Player.jumpProgress = 0;
+				Player.baseY = platform[2] - Player.playerHeight;
+				Player.y = Player.baseY;
+				Player.onBarrier = true;
 			}
-			if(Player.movingRight) {
-				if(Player.x < 100 - Player.playerWidth) {
-					var testX = Player.x + Player.playerWidth;
-					var testY = Player.y + Player.playerHeight;
-					if (!Barriers.insideBarrier(testX, testY)) {
-						Player.x += Player.sidewaysAmount;
-					} 
-				} else {
-					Player.x = 100 - Player.playerWidth;
-					Player.movingRight = false;
+			else if(Player.jumpProgress < 100){
+				Player.jumpProgress++;
+				var h = Player.upwardsAmount;
+				var p = Player.jumpProgress;
+				Player.y = Player.baseY - ((-h/2500) * (p * p) + (h/25) * p);
+			}
+			else {
+				Player.jumping = false;
+				Player.jumpProgress = 0;
+				if (!platform && Player.onBarrier) {
+					Player.onBarrier = false;
+					Player.falling = true;
+					Player.fallProgress = 100;
+					Player.y = Player.baseY - ((-h/2500) * (p * p) + (h/25) * p);
 				}
 			}
 		}
 		
+	},
+	placeOnGround: function() {
+		Player.falling = false;
+		Player.jumping = false;
+		Player.jumpProgress = 0;
+		Player.fallProgress = 0;
+		Player.onBarrier = false;
+		Player.baseY = Player.groundY;
+		Player.y = Player.baseY;
 	}
 };
 
 var Barriers = {
 	list: [],
-	addBarrier: function(x1, y1, x2, y2){
-		var b = [x1, y1, x2, y2];
+	addBarrier: function(x1, x2, y){
+		var b = [x1, x2, y];
 		Barriers.list.push(b);
 	},
 	insideBarrier: function(x, y){
 		for(var i = 0;i < Barriers.list.length; i++){
 			var b = Barriers.list[i];
-			if(x >= b[0] && x <= b[2] && y >= b[1] && y <= b[3]){
+			if(x >= b[0] && x <= b[1] && y >= b[2] && y <= b[2] + 2){
 				return b;
 			}
 		}
